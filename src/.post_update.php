89f436efe60ca59580284c9476954759e5b9860b
shiro-pit <slimyslime777@gmail.com>
2022-05-18 14:14:49 +0800

    Update .post_update.php

--- src/.post_update.php
+++ src/.post_update.php
@@ -1,7 +1,12 @@
 <?php
+$dir = str_replace('\vendor\rguj\laracore\src', '', __DIR__) . '\app\Helper';
+$file = $dir . '\BaseHelper.php';
 
-if(!file_exists('../../../app/Helper'))
-	throw new exception('Target directory doesn\'t exists');
+if(!file_exists($dir))
+	throw new exception('Target directory doesn\'t exists!');
 
-if(!copy('./src/Helper/BaseHelper.php', '../../../app/Helper/BaseHelper.php'))
+if(!copy('./Helper/BaseHelper.php', $file))
 	throw new exception('Copy failed');
+
+echo 'BaseHelper.php copied successfully!';
+
