6dac81b416d13b3c095c841e1ddaca26a7ad3cd1
Ro3h <69418277+rguj@users.noreply.github.com>
2023-03-30 17:00:47 +0800

    Update BaseHelper.php

--- src/Helper/BaseHelper.php
+++ src/Helper/BaseHelper.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\App;
 
 use Carbon\Carbon;
 use Carbon\CarbonImmutable;
@@ -3008,6 +3009,57 @@ function obj_reflect($obj, bool $to_array = false, $default = null)
     $reflected = $reflector($obj, $to_array) ?? $default;
     return $reflected;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** -----------------------------------------------
+ * REQUEST
+ */
+
+/**
+ * Redirect the user no matter what. No need to use a return
+ * statement. Also avoids the trap put in place by the Blade Compiler.
+ *
+ * @param string $url
+ * @param int $code http code for the redirect (should be 302 or 301)
+ */
+function redirect_now($url, $code = 302)
+{
+    try {
+        App::abort($code, '', ['Location' => $url]);
+    } catch (\Exception $exception) {
+        // the blade compiler catches exceptions and rethrows them
+        // as ErrorExceptions :(
+        //
+        // also the __toString() magic method cannot throw exceptions
+        // in that case also we need to manually call the exception
+        // handler
+        $previousErrorHandler = set_exception_handler(function () { });
+        restore_error_handler();
+        call_user_func($previousErrorHandler, $exception);
+        die;
+    }
+}
+
+
+
+
+
+
+
 
 
 
